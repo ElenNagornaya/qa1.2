c48bdb3539313576f9332491ad963712b1efd6bc
Elena Nagornaya <elen_nagornaya@mail.ru>
2023-11-04 17:51:02 +0300

    changed main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+       int ticket = 20_315;
+       int mile = 20;
 
+       int bonus = ticket / mile;
+
+        System.out.println("Итого начислено миль: " + bonus);
     }
 }
