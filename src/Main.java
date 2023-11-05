b4d522a767c6cf09bc140b1ca485ecbb12f62c6f
Elena Nagornaya <elen_nagornaya@mail.ru>
2023-11-05 14:46:15 +0300

    formatting main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-       int ticket = 20_315;
-       int mile = 20;
+        int ticket = 20_315;
+        int mile = 20;
 
-       int bonus = ticket / mile;
+        int bonus = ticket / mile;
 
         System.out.println("Итого начислено миль: " + bonus);
     }
